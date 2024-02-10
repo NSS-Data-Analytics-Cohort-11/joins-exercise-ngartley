ae21a1fa7b5f0c474bc06cb2101ff195105d1e0c
ngartley <nyssa.gartleyr@gmail.com>
2024-02-10 13:53:48 -0600

    finished 6 working on 7

---
+++ scripts/joins_movie_script.sql
@@ -0,0 +1,86 @@
+-- Question 1 - Give the name, release year, and worldwide gross of the lowest grossing movie.
+
+SELECT specs.film_title, specs.release_year, revenue.worldwide_gross
+FROM specs
+INNER JOIN revenue
+ON specs.movie_id = revenue.movie_id
+ORDER BY revenue.worldwide_gross
+
+-- Answer: Semi-Tough, 1977, 37187139
+
+-- Question 2 - What year has the highest average imdb rating?
+
+SELECT DISTINCT specs.release_year, AVG(rating.imdb_rating) AS avg_imdb_rating
+FROM specs
+INNER JOIN rating
+ON specs.movie_id = rating.movie_id
+GROUP BY specs.release_year
+ORDER BY avg_imdb_rating DESC
+
+-- Answer: 1991
+
+-- Question 3 - What is the highest grossing G-rated movie? Which company distributed it?
+
+SELECT specs.film_title, specs.mpaa_rating, revenue.worldwide_gross, distributors.company_name
+FROM revenue
+INNER JOIN specs
+	ON revenue.movie_id = specs.movie_id
+INNER JOIN distributors
+	ON specs.domestic_distributor_id = distributors.distributor_id
+WHERE specs.mpaa_rating = 'G'
+ORDER BY revenue.worldwide_gross DESC
+
+-- Answer: Toy Story 4 from Walt Disney
+
+-- Question 4 - Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
+
+SELECT distributors.company_name, COUNT(specs.film_title)
+FROM specs
+FULL JOIN distributors
+ON specs.domestic_distributor_id = distributors.distributor_id
+GROUP BY distributors.company_name
+
+-- Question 5 - Write a query that returns the five distributors with the highest average movie budget.
+
+SELECT distributors.company_name, AVG(revenue.film_budget) AS avg_film_budget
+FROM distributors
+FULL JOIN specs
+	ON distributors.distributor_id = specs.domestic_distributor_id
+LEFT JOIN revenue
+	ON specs.movie_id = revenue.movie_id
+WHERE revenue.film_budget IS NOT NULL
+GROUP BY distributors.company_name
+ORDER BY avg_film_budget DESC
+LIMIT 5;
+
+-- Question 6 - How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
+
+SELECT specs.film_title, rating.imdb_rating
+FROM distributors
+LEFT JOIN specs
+	ON distributors.distributor_id = specs.domestic_distributor_id
+LEFT JOIN rating
+	ON specs.movie_id = rating.movie_id
+WHERE distributors.headquarters NOT ilike '%CA'
+	AND specs.film_title IS NOT NULL
+ORDER BY rating.imdb_rating DESC
+
+-- Answer: 2. Dirty Dancing has the highest IMDB rating of 7.
+
+-- Question 7 - Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
+
+SELECT specs.length_in_min, AVG(rating.imdb_rating) AS avg_imdb_rating
+FROM specs
+LEFT JOIN rating
+	ON specs.movie_id = rating.movie_id
+WHERE specs.length_in_min > 120
+GROUP BY specs.length_in_min
+ORDER BY avg_imdb_rating
+
+SELECT specs.length_in_min, AVG(rating.imdb_rating) AS avg_imdb_rating
+FROM specs AS specs_left
+INNER JOIN specs AS specs_right
+	ON specs.movie_id = rating.movie_id
+	WHERE specs_left.length_in_min > 120
+GROUP BY specs_left.length_in_min
+ORDER BY avg_imdb_rating
